157482a4cdf067e5ae8c1a84ee9510f56aed2a92
sajker <sajkerprog@gmail.com>
2019-02-11 21:12:38 +0300

    first

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <v-footer :fixed="true">
+            <span>Ich Lerne Deutsch &copy; 2019</span>
+        </v-footer>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+
+</style>
---
+++ src/config/firebase.js
@@ -0,0 +1,10 @@
+export default {
+    
+        apiKey: "AIzaSyDZ9ndyN1pa4EKYTVRtZ6b1kh9x6KThRQc",
+        authDomain: "ichlernedeutsch-799fc.firebaseapp.com",
+        databaseURL: "https://ichlernedeutsch-799fc.firebaseio.com",
+        projectId: "ichlernedeutsch-799fc",
+        storageBucket: "ichlernedeutsch-799fc.appspot.com",
+        messagingSenderId: "537471005280"
+
+}
